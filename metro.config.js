db66f8371605137df7bd3659c6f1b8a2bac3f7d3
ajp-sourceship <ajp2914@gmail.com>
2026-09-19 23:58:45 -0500

    Remove unused path import and local module from Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
 const { withSentryConfig } = require('@sentry/react-native/metro');
 
 /**
@@ -11,9 +10,6 @@ const { withSentryConfig } = require('@sentry/react-native/metro');
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
-// Add the local react-native-share-sms module to watchFolders
-const localShareSmsPath = path.resolve(__dirname, '../react-native-share-sms');
-
 const config = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
@@ -24,9 +20,6 @@ const config = {
     // Enable symlink resolution
     resolveRequest: null,
   },
-  watchFolders: [
-    localShareSmsPath,
-  ],
 };
 
 module.exports = withSentryConfig(mergeConfig(defaultConfig, config));
